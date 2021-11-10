47a6b2a0eabc80e543a44b754fb7a320a36d2ae2
김윤태 <qndbsxo@naver.com>
2021-11-10 15:41:30 +0900

    인스타그램 해시태그 수집 및 분석

--- func.py
+++ func.py
@@ -2,56 +2,50 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 import re
-import time
-import re
-from bs4 import BeautifulSoup
 
 
-
-# 검색어어 조건에 따른 url 생성
+# 검색어 조건에 따른 url 생성
 def insta_searching(word):
     url = "https://www.instagram.com/explore/tags/" + str(word)
     return url
 
+
 # 열린 펭지에서 첫 번째 게시물 클릭 + sleep 메소드 통하여 시차 두기
 def select_first(driver):
-        first = driver.find_elements_by_css_selector("div._9AhH0")[0]
-        first.click()
-        time.sleep(3)
+    first = driver.find_elements_by_css_selector("div._9AhH0")[0]
+    first.click()
+    time.sleep(3)
 
 
 # 본문 내용, 작성일자, 좋아요 수, 위치 정보, 해시태그 가져오기
-
-
 def get_content(driver):
     html = driver.page_source
-    soup = BeautifulSoup(markup=html, features='lxml')
+    soup = BeautifulSoup(markup=html, features="lxml")
     # 본문 내용
     try:
-        content = soup.select(selector='div.C4VMK > span')[0].text
+        content = soup.select(selector="div.C4VMK > span")[0].text
     except:
-        content = ' '
+        content = " "
     # 해시태그
-    tags = re.findall(pattern=r'#[^\s#,\\]+', string=content)
-    # 작성일자 
-    date = soup.select(selector='time._1o9PC.Nzb55')[0]['datetime'][:10]
+    tags = re.findall(pattern=r"#[^\s#,\\]+", string=content)
+    # 작성일자
+    date = soup.select(selector="time._1o9PC.Nzb55")[0]["datetime"][:10]
     # 좋아요
     try:
-        like = soup.select(selector='div.Nm9Fw > button')[0].text[4:-1]
+        like = soup.select(selector="div.Nm9Fw > button")[0].text[4:-1]
     except:
         like = 0
     # 위치
     try:
-        place = soup.select(selector='div.M30cS')[0].text
+        place = soup.select(selector="div.M30cS")[0].text
     except:
-        place = ''
+        place = ""
     data = [content, date, like, place, tags]
     return data
 
+
 # 첫 번째 게시물 클릭 후 다음 게시물 클릭
 def move_next(driver):
-    right = driver.find_element_by_css_selector('a.coreSpriteRightPaginationArrow')
+    right = driver.find_element_by_css_selector("a.coreSpriteRightPaginationArrow")
     right.click()
     time.sleep(3)
-
-    
--- selenium_insta.py
+++ selenium_insta.py
@@ -1,16 +1,11 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
-import re
-import time
 from func import *
 
-# 오류 해결 라라
+# 오류 해결
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
-# driver = webdriver.Chrome(options=options)
-
-
 
 
 # 크롤링 시작
@@ -20,19 +15,18 @@ target 변수에 크롤링할 게시글의 수를 바인딩
 """
 
 # 크롬 부라우저 열기
-driver = webdriver.Chrome(executable_path='chromedriver.exe', options=options)
-
-driver.get(url='https://www.instagram.com')
+driver = webdriver.Chrome(executable_path="chromedriver.exe", options=options)
+driver.get(url="https://www.instagram.com")
 time.sleep(3)
 
 # 인스타그램 로그인을 위한 계정 정보
-email = 'qndbsxo@gmail.com'
-input_id = driver.find_elements_by_css_selector('input._2hvTZ.pexuQ.zyHYP')[0]
+email = "qndbsxo@gmail.com"
+input_id = driver.find_elements_by_css_selector("input._2hvTZ.pexuQ.zyHYP")[0]
 input_id.clear()
 input_id.send_keys(email)
 
 password = "wasdas5257!"
-input_pw = driver.find_elements_by_css_selector('input._2hvTZ.pexuQ.zyHYP')[1]
+input_pw = driver.find_elements_by_css_selector("input._2hvTZ.pexuQ.zyHYP")[1]
 input_pw.clear()
 input_pw.send_keys(password)
 input_pw.submit()
@@ -40,7 +34,7 @@ input_pw.submit()
 time.sleep(5)
 
 # 게시물을 조회할 검색 키워드 입력 요청
-word = input('검색어를 입력하세요 :')
+word = input("검색어를 입력하세요 :")
 word = str(word)
 url = insta_searching(word)
 
@@ -74,7 +68,6 @@ print(results[:2])
 import pandas as pd
 
 result_df = pd.DataFrame(data=results)
-result_df.columns = ['content', 'date', 'like', 'place', 'tags']
-result_df.to_excel('koreanfood.xlsx')
-
+result_df.columns = ["content", "date", "like", "place", "tags"]
+result_df.to_excel("koreanfood.xlsx")
 
